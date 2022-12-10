d6f9618c4febcb58fb78db0fc6b30f8dfe3ee338
Jason Myerscough <jmyerscough1@googlemail.com>
2022-12-10 23:43:28 +0100

    day 3 solution part 2
    day 3 solution part 2

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,18 +1,36 @@
-﻿
 var filename = "input.txt";
 var lines = System.IO.File.ReadAllText(filename);
 var priorities = 0;
 const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 foreach(var line in lines.Split("\n")) {
-    var s1 = line.Substring(0, line.Length / 2);
-    var s2 = line.Substring(line.Length / 2);
+    priorities += part1(line);
+}
+Console.WriteLine($"Sum of priorities: {priorities}");
+
+part2(lines.Split("\n"));
+
+int part1(string s) {
+    var s1 = s.Substring(0, s.Length / 2);
+    var s2 = s.Substring(s.Length / 2);
 
     foreach (var c in s1) {
         if (s2.Contains(c)) {
-            priorities += letters.IndexOf(c) + 1;
-            break;
+            return letters.IndexOf(c) + 1;
         }
     }
+    return 0;
 }
-Console.WriteLine($"Sum of priorities: {priorities}");
+
+void part2(string [] lines) {
+    var r = 0;
+    for (int i = 0; i <= lines.Length - 3; i+=3) {
+        foreach (var c in lines[i]) {
+            if (lines[i+1].Contains(c) && lines[i+2].Contains(c)) {
+                r += letters.IndexOf(c) + 1;
+                break;
+            }
+        }
+    }
+    Console.WriteLine($"Part 2 priority: {r}");
+}
