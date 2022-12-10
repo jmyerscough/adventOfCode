b0f544a7052335c35b5c400c81a81d72791c147a
Jason Myerscough <jmyerscough1@googlemail.com>
2022-12-10 01:56:47 +0100

    Add files via upload
    day 01 solution

---
+++ day01/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+var filename = "input.txt";
+var lines = System.IO.File.ReadAllText(filename);
+var elves = new List<int>();
+
+var calorieCount = 0;
+var maxCount = 0;
+
+foreach(var line in lines.Split("\n")) {
+    if(line.ToString().Length > 0) {
+        var c = 0;
+        if (Int32.TryParse(line.ToString(), out c)) {
+            calorieCount += c;
+        }
+    }
+    else
+    {
+        elves.Add(calorieCount);
+        if (calorieCount > maxCount) {
+            maxCount = calorieCount;
+        }
+        calorieCount = 0;
+    }
+}
+
+elves.Sort();
+var top3 = elves[elves.Count() - 1] + elves[elves.Count() - 2] + elves[elves.Count() - 3];
+
+
+Console.WriteLine($"Max Calorie Count: {maxCount}");
+Console.WriteLine($"Top 3: {top3}");
