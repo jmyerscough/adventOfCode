5bfba4b7291fdca88074fb160615ad78fc7309ce
Jason Myerscough <jmyerscough1@googlemail.com>
2022-12-10 14:17:52 +0100

    solution to part 2
    solution to part 2

--- day02/Program.cs
+++ day02/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 var filename = "input.txt";
 var lines = System.IO.File.ReadAllText(filename);
 
@@ -12,26 +12,48 @@ var moves = new Dictionary<string, Move>() {
 };
 
 var myScore = 0;
+var myScorePart2 = 0;
 foreach(var line in lines.Split("\n")) {
-    var players = line.Split(" ");
+    myScore += part1(line);
+    myScorePart2 += part2(line);
+}
+
+Console.WriteLine($"Part 1: {myScore}, Part 2: {myScorePart2}");
+
+int part1(string s) {
+    var players = s.Split(" ");
     var computer = moves[players[0]];
     var me =  moves[players[1]];
 
     if (computer == me) {
-        myScore += ((int)me) + ((int)GameResult.Draw);
+        return ((int)me) + ((int)GameResult.Draw);
     }
     else if (computer == Move.Rock && me == Move.Paper) {
-        myScore += ((int)GameResult.Win) + ((int)me);
+        return ((int)GameResult.Win) + ((int)me);
     }
     else if (computer == Move.Scissors && me == Move.Rock) {
-        myScore += ((int)GameResult.Win) + ((int)me);
+        return ((int)GameResult.Win) + ((int)me);
     }
     else if (computer == Move.Paper && me == Move.Scissors) {
-        myScore += ((int)GameResult.Win) + ((int)me);
+        return ((int)GameResult.Win) + ((int)me);
     }
     else {
-        myScore += ((int)me);
+        return ((int)me);
     }
 }
 
-Console.WriteLine(myScore);
+int part2(string s) {
+    var players = s.Split(" ");
+    var computer = moves[players[0]];
+    var me =  moves[players[1]];
+
+    switch (players[1]) {
+        case "X": 
+            return  computer == Move.Rock ? ((int)Move.Scissors) : computer == Move.Paper ? ((int)Move.Rock) : ((int)Move.Paper);
+        case "Y": return ((int)GameResult.Draw) + ((int)computer);
+        case "Z": 
+            return  computer == Move.Rock ? ((int)Move.Paper) + ((int)GameResult.Win) : computer == Move.Paper ?
+                ((int)Move.Scissors) + ((int)GameResult.Win) : ((int)Move.Rock) + ((int)GameResult.Win);
+    }
+    return 0;
+}
