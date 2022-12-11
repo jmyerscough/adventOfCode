ba18fcc7965a9640e1cb56094653bdc834d7dab3
Jason Myerscough <jmyerscough1@googlemail.com>
2022-12-11 20:06:04 +0100

    day 4 solution part 2
    day 4 solution part 2

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,8 +1,8 @@
-﻿
 var filename = "input.txt";
 var lines = System.IO.File.ReadAllText(filename);
 
 var containsCount = 0;
+var overlapCount = 0;
 
 foreach(var line in lines.Split("\n")) {
     var range1 = line.Split(",")[0];
@@ -10,6 +10,9 @@ foreach(var line in lines.Split("\n")) {
 
     var s1 = buildRange(range1);
     var s2 = buildRange(range2);
+
+    if (part2(range1, range2))
+        ++overlapCount;
 
     if (s1.Length == s2.Length) {
         if (s1 == s2) ++containsCount;
@@ -28,6 +31,7 @@ foreach(var line in lines.Split("\n")) {
     }
 }
 Console.WriteLine($"containsCount = {containsCount}");
+Console.WriteLine($"overlapCount = {overlapCount}");
 
 string buildRange(string s) {
     var start = int.Parse(s.Split("-")[0]);
@@ -41,4 +45,32 @@ string buildRange(string s) {
         }
     }
     return result;
+}
+
+// dirty, don't like my implementation.
+bool part2(string range1, string range2) {
+    var r1 = new List<int>();
+    var r2 = new List<int>();
+
+    var start1 = int.Parse(range1.Split("-")[0]);
+    var end1 = int.Parse(range1.Split("-")[1]);
+    var start2 = int.Parse(range2.Split("-")[0]);
+    var end2 = int.Parse(range2.Split("-")[1]);
+
+    for (int i=start1; i <= end1; i++)
+        r1.Add(i);
+
+    for (int i=start2; i <= end2; i++)
+        r2.Add(i);
+
+    if (r1.Count >= r2.Count) {
+        foreach (var n in r2)
+            if (r1.Contains(n))
+                return true;
+    } else {
+        foreach (var n in r1)
+            if (r2.Contains(n))
+                return true;
+    }
+    return false;
 }
