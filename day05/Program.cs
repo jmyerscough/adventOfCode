3f50768a4f38b0731f797bfdde68d9132c4af456
Jason Myerscough <jmyerscough1@googlemail.com>
2022-12-15 23:36:14 +0100

    day 5, parsing the stacks.
    day 5, parsing the stacks.

---
+++ day05/Program.cs
@@ -0,0 +1,31 @@
+﻿var filename = "input.txt";
+var lines = System.IO.File.ReadAllText(filename);
+
+parseStacks(lines.Split("\n"));
+
+
+List<Stack<char>> parseStacks(string [] input) {
+    var i = 0;
+
+    // read the list of stacks
+    while (input[i].Length > 1) {
+        ++i;
+    }
+    var indices = input[i-1];
+
+    // get the number of stacks
+    var stackCount = int.Parse(indices.Trim()[indices.Trim().Length -1].ToString());
+    var stacks = new List<Stack<char>>(stackCount);
+
+    for (var j=0; j < stackCount; j++) stacks.Add(new Stack<char>());
+ 
+    // read the stacks
+    for (var j=i-2; j >= 0; j--) {
+        for (var k = 1; k <= stackCount; k++) {
+            var idx = indices.IndexOf((k.ToString()));
+            if (Char.IsLetter(input[j][idx]))
+                stacks[k-1].Push((input[j][idx]));
+        }
+    }
+    return stacks;
+}
